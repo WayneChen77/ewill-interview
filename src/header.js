1d2e736b9dc0ead67583f1b5abf43d04d4c23e0f
wayne chen <b126015637@gmail.com>
2022-09-27 22:43:07 +0800

    header1

---
+++ src/header.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="txtandbtn">
+        <h1>There is no one</h1>
+        <p>who loves pain</p>
+        <button className="btn">FORM</button>
+      </div>
+
+      <div className="content">
+        <div className="con1 con">
+          <h3>parapraph</h3>
+          <ol>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. um hic
+              harum officiis.
+            </li>
+          </ol>
+        </div>
+        <div className="con2 con">
+          <h3>parapraph</h3>
+          <ol>
+            <li>
+              Quisque sodales leo <strong>vitae vulputate auctor.</strong>
+            </li>
+            <li>
+              Proin ac justo ut nisl tincidunt imperdiet.Maecenas viverra libero
+              a pellentesque blandit.
+            </li>
+            <li>
+              Cras tristique tellus id leo bibendum, eu dapibus nisl accumsan.
+            </li>
+            <li>
+              Donec ultrices sapien
+              <strong>vitae leo venenatis ullamcorper.</strong>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
